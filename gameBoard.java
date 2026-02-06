08d7fdbfec1a1826c467b13b0359acb2a88db827
JoshuaLCary <caryj5@wwu.edu>
2026-02-05 19:02:28 -0800

    Starter Set Up for Classes + Links in Readme for Info

---
+++ gameBoard.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+public class gameBoard {
+
+    int diceNum;
+    Random r = new Random();
+
+    // We want roll dice to return a list sorted in descending order 
+    //so it can be passes amount amongst the players
+    public int[] rollDice(int diceNum) {
+        int [] rolledDice = new int[diceNum];
+        for(int i = 0; i < diceNum; i++) {
+            int die = r.nextInt(6);
+            // to convert form 0-5 to 1-6
+            rolledDice[i] = die +1;
+        }
+        return rolledDice;
+    }
+
+    
+}
