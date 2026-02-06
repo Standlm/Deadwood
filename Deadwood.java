ff921fbccfa7f9f2e4a4fcd951dd35345283e7f6
JoshuaLCary <caryj5@wwu.edu>
2026-02-06 14:40:09 -0800

    Deadwood main method and goals for Friday, Feb 6th

--- Deadwood.java
+++ Deadwood.java
@@ -1,3 +1,11 @@
 public class Deadwood {
-    
+    public static void main(String[] args) {
+        // Run Board Space Example
+
+        // Run Roll Example
+
+        // Eventually, run GameBoard
+    }
+
+
 }
