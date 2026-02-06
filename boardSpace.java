2266b1857d9de5828847f387600d04e016d7e3a1
JoshuaLCary <caryj5@wwu.edu>
2026-02-05 21:47:22 -0800

    boardSpace update

--- boardSpace.java
+++ boardSpace.java
@@ -5,6 +5,9 @@ public class boardSpace {
     private String id;
     private String type;
 
+    // Turn into list or something of neighbors
+    private String neighbors;
+
     public static void main(String[] args) {
         boardSpace bSpace = new boardSpace(args[0], args[1]);
         System.out.println("The id of this role is: " + bSpace.id + ".\n" + "The type of role: " + bSpace.type);
