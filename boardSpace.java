01d1fa957b9859c2fb57ca65a3e3fd79ee3ccb2e
JoshuaLCary <caryj5@wwu.edu>
2026-02-05 21:14:37 -0800

    added values and constructor

--- boardSpace.java
+++ boardSpace.java
@@ -1,8 +1,17 @@
-import java.util.Random;
+
+
 public class boardSpace {
 
+    private String id;
+    private String type;
+
     public static void main(String[] args) {
-        // something
+        boardSpace bSpace = new boardSpace(args[0], args[1]);
+        System.out.println("The id of this role is: " + bSpace.id + ".\n" + "The type of role: " + bSpace.type);
     }
 
+    public boardSpace(String spaceId, String spaceType) {
+        this.id = spaceId; // use 'this' to refer to the class variable
+        this.type = spaceType;
+    }
 }
