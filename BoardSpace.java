65b078c6267a1d12c4d5727f70807f2c57d559e9
JoshuaLCary <caryj5@wwu.edu>
2026-02-06 19:57:14 -0800

    Added print method in BoardSpace

--- BoardSpace.java
+++ BoardSpace.java
@@ -15,17 +15,20 @@ public class BoardSpace {
         String[] neighbors = {"General Store", "Train Station", "Main Street"};
         // board space concept
         BoardSpace bSpace = new BoardSpace(id, type, neighbors);
-        System.out.println("The id of this space is: " + bSpace.id + ".\n" + "The type of space: " + bSpace.type);
-
-        System.out.println("Here are the neighbors of this space:");
-        for (int i = 0; i < bSpace.neighbors.length; i++){
-            System.out.println((i + 1)  + ". " + bSpace.neighbors[i]);
-        }
+        printBoardSpace(bSpace);
     }
 
     public BoardSpace(String spaceId, String spaceType, String[] spaceNeighbors) {
         this.id = spaceId; // use 'this' to refer to the class variable
         this.type = spaceType;
         this.neighbors = spaceNeighbors;
+    }
+
+    public static void printBoardSpace(BoardSpace boardSpace){
+        System.out.println("The id of this space is: " + boardSpace.id + ".\n" + "The type of space: " + boardSpace.type);
+        System.out.println("Here are the neighbors of this space:");
+        for (int i = 0; i < boardSpace.neighbors.length; i++){
+            System.out.println((i + 1)  + ". " + boardSpace.neighbors[i]);
+        }
     }
 }
