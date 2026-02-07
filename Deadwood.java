df87e3446f3c1e5bc31ba372a96b5b7c49ee4a07
JoshuaLCary <caryj5@wwu.edu>
2026-02-06 20:28:18 -0800

    clean up in Deadwood.java

--- Deadwood.java
+++ Deadwood.java
@@ -23,14 +23,6 @@ public class Deadwood {
         //Role example for test cases
         Role roleExample = new Role(rolename, rolerank, roletype);
         Role.printRole(roleExample);
-     
-    
-     
-
-
- 
-
-        // Run Roll Example
 
         // Eventually, run GameBoard
     }
