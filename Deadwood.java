c0b283e934c783e562d9811e1597f1e881c6caa6
standlm <standlm@cf164-00.cs.wwu.edu>
2026-02-06 16:14:37 -0800

    switch computers just making sure everything is up to date

--- Deadwood.java
+++ Deadwood.java
@@ -1,10 +1,15 @@
+import java.util.Scanner;
 
 public class Deadwood {
 
     public static void main(String[] args) {
-         LoadXml xmlloader = new LoadXml();
-        xmlloader.LoadBoard();
-        xmlloader.LoadCard();
+
+        // only instantiate once
+        Scanner myObj = new Scanner(System.in);
+
+     
+    
+     
 
 
         // Run Board Space Example
@@ -12,6 +17,49 @@ public class Deadwood {
         // Run Roll Example
 
         // Eventually, run GameBoard
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    private static void print(String toPrint) {
+        char[] chars = toPrint.toCharArray();
+        for (int i=0; i < chars.length; i++) {
+            System.out.print(chars[i]);
+            try { Thread.sleep(25);} 
+            catch (InterruptedException e) {Thread.currentThread().interrupt();}
+        }
+        System.out.println("");
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
     }
 
 
