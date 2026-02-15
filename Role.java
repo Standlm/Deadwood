2fec91b601ced8c0dfb8271b23611ac7a8263a90
JoshuaLCary <caryj5@wwu.edu>
2026-02-14 20:33:01 -0800

    Scene mostly finished, role gained getters and practiceShots

--- Role.java
+++ Role.java
@@ -3,32 +3,53 @@ public class Role {
     private String type;
     private int rank;
     private String name;
-    // roles get sent in Name, rank, type
+    private int practiceShots;
+
+    // roles get sent in Name, rank, type, praticeShots
     public Role( String name, int rank, String type){
         this.name = name;
         this.rank = rank;
         this.type = type;
-
+        this.practiceShots = 0;
     }
-       public static void main(String[] args) {
+
+    public static void main(String[] args) {
         String type = "On-Card";
         String name = "Marshal Canfield";
         int rank = 3;
         //Role example for test cases
         Role roleExample = new Role(name, rank, type);
-        printRole(roleExample);
-        
+        printRole(roleExample);   
     }
 
+    // Getters
+    public String getName(){
+        return name;
+    }
+    public int getRank(){
+        return rank;
+    }
+    public String getType(){
+        return type;
+    }
+    public int getPracticeShots(){
+        return practiceShots;
+    }
 
+    // local value changer for practiceShots
+    public void rehearse(){
+        practiceShots++;
+    }
 
 
     public static void printRole(Role role ) {
-        System.out.println("This is a " + role.type + " role \n");
-        System.out.println("This role is playing as " + role.name + "\n");
-        System.out.println("This role is level " +role.rank + "\n");
+        System.out.println("This is a " + role.type + " role");
+        System.out.println("This role is playing as " + role.name);
+        System.out.println("This role is level " + role.rank);
+        System.out.println("Current practice shots: " + role.practiceShots);
 
     }
+
     public boolean isOnCard(){
     if ( this.type.equals("Featured") == true ){
             return true;
--- Scene.java
+++ Scene.java
@@ -4,23 +4,50 @@ public class Scene {
     private String sceneName;
     private int budget;
     private Role[] roles;
+    private int shotsLeft;
+
+    public static void main(String[] args) {
+        
+    }
 
     // Scene Constructor just to heintlp understand Role more
-    public Scene(String locationID, String sceneName, int budget, Role[] roles){
+    public Scene(String locationID, String sceneName, int budget, Role[] roles, int shots){
         this.locationID = locationID;
         this.sceneName = sceneName;
         this.budget = budget;
         this.roles = roles;
+        this.shotsLeft = shots;
     }
-      public static void main(String[] args) {
-        
-    }
-    // act
-    
 
+    // Getters
+    public String getSceneName(){
+        return sceneName;
+    }
+    public int getBudget(){
+        return budget;
+    }
+    public int getShotsLeft(){
+        return shotsLeft;
+    }
     
     // act
+    // Requires Dice class to give total
+    public boolean act(Role role, int diceTotal){
+        if (shotsLeft > 0 && (diceTotal + role.getPracticeShots()) >= budget){
+            shotsLeft--;
+            return true;
+        } 
+        return false;
+    }
+
+    // rehearse
+    public void rehearse(Role role){
+        role.rehearse();
+    }
     
-    //rehearse
+    // wrap
+    public boolean isWrapped() {
+        return shotsLeft >= 0;
+    }
 
 }
